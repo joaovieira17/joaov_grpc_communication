c289e5bb5872fa3d6b684f2c441c89b2911c222b
joaovieira17 <1201005@isep.ipp.pt>
2023-06-08 01:21:09 +0100

    coisas

---
+++ gRPC/Auth/src/test/java/com/authorization/model/UserTest.java
@@ -0,0 +1,63 @@
+package com.authorization.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UserTest {
+
+    @Test
+    void ensureUserNameIsAccepted() {
+        final User user = new User("Albert","123");
+        assertEquals("Albert", user.getUsername());
+        User user1 = new User();
+        user1.setUsername("Albert");
+        assertEquals("Albert", user.getUsername());
+    }
+
+    @Test
+    void ensurePasswordIsAccepted() {
+        final User user = new User("Albert","123");
+        assertEquals("123", user.getPassword());
+        User user1 = new User();
+        user1.setPassword("123");
+        assertEquals("123", user.getPassword());
+    }
+
+    @Test
+    void ensureEmailIsSet() {
+        final User user = new User("Albert","123");
+        user.setEmail("albert@gmail.com");
+        assertEquals("albert@gmail.com", user.getEmail());
+    }
+
+    @Test
+    void ensureAddressIsSet() {
+        final User user = new User("Albert","123");
+        user.setAddress("Avenida dos Platanos");
+        assertEquals("Avenida dos Platanos", user.getAddress());
+    }
+
+    @Test
+    void ensureEverythingIsAcceptedAndInPlace() {
+        final User user = new User("Albert","123", "albert@gmail.com","Avenida dos Platanos");
+        assertEquals("Albert", user.getUsername());
+        assertEquals("123", user.getPassword());
+        assertEquals("albert@gmail.com", user.getEmail());
+        assertEquals("Avenida dos Platanos", user.getAddress());
+    }
+
+    @Test
+    void ensureRoleIsGiven() {
+        final User user = new User("Albert","123", "albert@gmail.com","Avenida dos Platanos");
+        user.addAuthority(new Role(Role.MODERATOR));
+        List<Role> roles= new ArrayList<>(user.getRoles());
+
+        assertEquals(new Role(Role.MODERATOR), roles.get(0));
+    }
+}
